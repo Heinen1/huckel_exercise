60ab8a06c31db6d1cfdad93ce6551638e9bd11d7
Heinen1 <jurnheinen111@gmail.com>
2022-08-13 17:53:01 +0200

    substitute H for hamiltonian

--- src/python/allyl_nosym.py
+++ src/python/allyl_nosym.py
@@ -1,13 +1,16 @@
 import time 
 import numpy as np
 from numpy import linalg as LA
-H = np.array([
+
+hamiltonian = np.array([
 [0, 1, 0],
 [1, 0, 1],
 [0, 1, 0]])
+
 time_begin = time.perf_counter()
-E, c = LA.eig(H)
+E, c = LA.eig(hamiltonian)
 time_end= time.perf_counter()
+
 print("Eigenvalues E: \n", E)
 print("Eigenvector c: \n", c)
 print("Elapsed time: ",time_end-time_begin)
--- src/python/allyl_sym.py
+++ src/python/allyl_sym.py
@@ -1,12 +1,15 @@
 import time 
 import numpy as np
 from numpy import linalg as LA
-H = np.array([
-[0, np.sqrt(2)],
-[np.sqrt(2), 0]])
+
+hamiltonian = np.array([
+[0,             np.sqrt(2)],
+[np.sqrt(2),    0]])
+
 time_begin = time.perf_counter()
-E, c = LA.eig(H)
+E, c = LA.eig(hamiltonian)
 time_end= time.perf_counter()
+
 print("Eigenvalues E: \n", E)
 print("Eigenvector c: \n", c)
 print("Elapsed time: ",time_end-time_begin)
